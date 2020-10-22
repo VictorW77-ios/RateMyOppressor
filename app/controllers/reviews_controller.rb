5b4506c49b4e88d61bdab2c592603c9a4ed53e17
“VictorW77-ios” <victorwilliams1030@gmail.com>
2020-10-22 14:43:24 -0500

    made #create, #new, and private #review_params

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,24 @@
 class ReviewsController < ApplicationController
+
+    before_action :redirect_if_not_logged_in
+
+
+    def new 
+        @review = Review.new 
+    end 
+
+    def create 
+        @review = current_user.reviews.build(review_params)
+        if @review.save
+            redirect_to reviews_path   # redirect to officer page with new review
+        else 
+            render :new
+        end 
+    end 
+
+    private 
+
+    def review_params 
+        params.require(:review).permit(:title, :content)
+    end 
 end
