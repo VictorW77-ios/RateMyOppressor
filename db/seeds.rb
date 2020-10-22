df06e00ed3944b141b55b9dfbece773252da91b3
“VictorW77-ios” <victorwilliams1030@gmail.com>
2020-10-22 14:47:19 -0500

    seeded fake test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+v = User.create(email: 'vic@vic.com', username: 'vic', password: '123')
+j = User.create(email: 'jai@jai.com', username: 'jai', password: '123')
