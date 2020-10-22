b054059d693c4dc0a48121322ff21bda6284df25
“VictorW77-ios” <victorwilliams1030@gmail.com>
2020-10-22 14:42:05 -0500

    created helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+
+    # allows me to access these methods in views
+    helper_method :current_user, :logged_in?
+
+    private 
+    def current_user
+        @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
+            # prevents it from making a call to the db if there isn't a user
+            # finds by id in user table
+            # memoization 
+    end 
+
+    def logged_in? 
+        !!session[:user_id]
+    end 
+
+    def redirect_if_not_logged_in
+        redirect_to '/' if !logged_in?
+    end 
 end
