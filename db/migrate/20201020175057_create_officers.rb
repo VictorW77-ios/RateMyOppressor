8195b2a418c5b42e63205e1757541cd1055b76dc
“VictorW77-ios” <victorwilliams1030@gmail.com>
2020-10-25 11:37:43 -0500

    working on updating officers table

--- db/migrate/20201020175057_create_officers.rb
+++ db/migrate/20201020175057_create_officers.rb
@@ -3,8 +3,20 @@ class CreateOfficers < ActiveRecord::Migration[6.0]
     create_table :officers do |t|
       t.string :name
       t.string :description
+      # t.string :race
+      # t.string :job_title
+      # t.string :gender 
+      # t.integer :badge_number
 
       t.timestamps
     end
+
+    # change_table :officers do |t|
+    #   t.remove :description
+    #   t.string :race
+    #   t.string :job_title 
+    #   t.string :gender
+    #   t.index :badge_number
+    # end
   end
 end
