16af305f07fdde264c6e5dfbe496a94d5fabc37a
“VictorW77-ios” <victorwilliams1030@gmail.com>
2020-10-25 11:37:14 -0500

    refactored #show and added #index

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -8,12 +8,21 @@ class ReviewsController < ApplicationController
     end 
 
     def create 
-        @review = current_user.reviews.build(review_params)
+        @review = current_user.reviews.build(review_params) # associates reviews with user // built off user
         if @review.save
             redirect_to reviews_path   # redirect to officer page with new review
         else 
             render :new
         end 
+    end 
+
+    def index 
+        @reviews = Review.all 
+    end 
+
+    def show 
+        @review = Review.find_by_id(params[:id])
+        redirect_to reviews_path if !@review
     end 
 
     private 
