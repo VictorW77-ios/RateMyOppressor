8df2f0507506f6a1a5d06b43fb892e7f07279d73
“VictorW77-ios” <victorwilliams1030@gmail.com>
2020-10-25 11:37:27 -0500

    working on nested routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
     has_many :reviews
     has_many :officers, through: :reviews
+    has_many :posted_reviews, through: :reviews, source: :officer
 
     has_secure_password
 end
+
+
+# --- INSERT NESTED ROUTES --- 
+'/users'
+'/officers'
+'/reviews'
