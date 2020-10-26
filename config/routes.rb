42aa2e2b8899ef1283a617c391f1479300629d1f
“VictorW77-ios” <victorwilliams1030@gmail.com>
2020-10-25 22:48:44 -0500

    refactored omniauth methods + updated comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,29 @@
 Rails.application.routes.draw do
+  # default route
+  root "welcome#home"
 
-  root "sessions#home"
-
+  # SIGNUP ROUTE 
   get '/signup' => 'users#new'
-
   post '/signup' => 'users#create'
 
+  # LOGIN ROUTE 
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
+
+  # LOGOUT ROUTE 
   delete '/logout' => 'sessions#destroy'
 
+  # Omni-Auth Route 
+  get "/google_auth/:provider/callback" => 'sessions#google_auth'
 
-  resources :officers
-  resources :users
-  resources :reviews
+  resources :officers do 
+    resources :reviews 
+  end 
+
+  resources :reviews 
+
+  resources :users do 
+    resources :reviews, shallow: true 
+  end 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
