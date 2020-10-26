8bfc5326932bf8983aa4ca170798d5e3b416b26e
“VictorW77-ios” <victorwilliams1030@gmail.com>
2020-10-25 22:42:25 -0500

    added get routes for #index and #show

--- app/controllers/officers_controller.rb
+++ app/controllers/officers_controller.rb
@@ -1,3 +1,25 @@
 class OfficersController < ApplicationController
-    # before_action :redirect_if_not_logged_in
+  before_action :set_user
+
+
+  def new 
+    @officer = Officer.new
+  end 
+
+    # GET /officers 
+  def index 
+    @officers = Officer.all
+  end 
+    
+    # GET /officers/1
+  def show 
+    @officer = Officer.find(params[:id])
+  end 
+
+  private 
+
+  def set_user
+    @user = current_user 
+  end 
+
 end
